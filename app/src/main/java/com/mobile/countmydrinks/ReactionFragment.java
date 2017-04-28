931dbed95868a5aeeadee60872ce4b101fcb0e0a
chrsrck <chrsrck292@gmail.com>
2017-04-28 19:10:06 -0400

    Calling and cancelling asynctask correctly, timing slightly off

--- app/src/main/java/com/mobile/countmydrinks/ReactionFragment.java
+++ app/src/main/java/com/mobile/countmydrinks/ReactionFragment.java
@@ -15,15 +15,19 @@ import android.widget.TextView;
 
 public class ReactionFragment extends Fragment {
 
-    private TouchListener mTouchListener;
-    private int touchCount;
-    private boolean gameStarted;
+    private static int MILLISECS_IN_SECOND = 1000;
+    private static int MILLISECS_IN_DECISECS = 100;
+    private static int MILLISECS_IN_CENTISECS = 10;
 
     private TextView timeCountText;
     private TextView promptText;
     private TextView baselineText;
 
-
+    private TouchListener mTouchListener;
+    private boolean gameStarted;
+    private int milliseconds;
+    private GametimeAsyncTask gametimeAsyncTask;
+    private StringBuilder mStringBuilder;
 
 
     @Nullable
@@ -35,29 +39,53 @@ public class ReactionFragment extends Fragment {
         promptText = (TextView) view.findViewById(R.id.promptText);
         baselineText = (TextView) view.findViewById(R.id.baselineText);
 
-        touchCount = 0;
         gameStarted = false;
         mTouchListener = new TouchListener(mainActivity);
         view.setOnTouchListener(mTouchListener);
-
+        mStringBuilder = new StringBuilder();
         return view;
     }
 
     public void onTouchFired() {
 
-        // end game game
+        // end game
         if (gameStarted) {
             promptText.setText(R.string.too_much_prompt);
             gameStarted = false;
             this.getView().setBackgroundColor(Color.WHITE);
+            gametimeAsyncTask.cancel(true);
         }
-        // start game game
+        // start game
         else {
             promptText.setText(R.string.go_prompt);
             gameStarted = true;
             this.getView().setBackgroundColor(Color.GREEN);
+            gametimeAsyncTask = new GametimeAsyncTask();
+            gametimeAsyncTask.execute();
         }
 
+    }
+
+    private void setTimeTextValues(int passedTime) {
+        int seconds = passedTime / MILLISECS_IN_SECOND;
+        int millisecs = passedTime - (seconds * MILLISECS_IN_SECOND);
+
+        if (seconds < 10) {
+            mStringBuilder.append("0");
+        }
+
+        mStringBuilder.append(Integer.toString(seconds) + ":");
+
+        if (millisecs < 100) {
+            mStringBuilder.append("0");
+        }
+
+        if (millisecs < 10) {
+            mStringBuilder.append("0");
+        }
+        mStringBuilder.append(Integer.toString(millisecs));
+        timeCountText.setText(mStringBuilder.toString());
+        mStringBuilder.setLength(0);
     }
 
     private void startGame() {
@@ -68,11 +96,41 @@ public class ReactionFragment extends Fragment {
 
     }
 
-    private class gametimeAsyncTask extends AsyncTask<Void, Integer, Void> {
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!gametimeAsyncTask.isCancelled()) {
+            gametimeAsyncTask.cancel(true);
+        }
+
+    }
+
+    private class GametimeAsyncTask extends AsyncTask<Void, Integer, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
+
+            while (gameStarted) {
+                try {
+                    Thread.sleep(1);
+                    milliseconds++;
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                publishProgress(new Integer(milliseconds));
+
+                if (isCancelled()) {
+                    break;
+                }
+            }
             return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            setTimeTextValues(values[0].intValue());
         }
     }
 
