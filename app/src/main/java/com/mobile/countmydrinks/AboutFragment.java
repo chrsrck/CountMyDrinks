0ae3d329c3510811732bc9c314f98091cb2f5301
Hanson Cress <Hanson Cress>
2017-04-30 13:00:29 -0400

    added the information to the about page

--- app/src/main/java/com/mobile/countmydrinks/AboutFragment.java
+++ app/src/main/java/com/mobile/countmydrinks/AboutFragment.java
@@ -6,13 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutFragment extends Fragment {
 
+TextView drinkInfo;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.about, container, false);
+        drinkInfo = (TextView) view.findViewById(R.id.drinkInfo);
+        drinkInfo.setText("Party Positive Zone BAC  <= .06 \n"+
+                "Legal Driving Zone BAC <= .08 \n"+
+                "Danger Zone BAC  <= .10 \n");
+
+
         return view;
     }
 }
