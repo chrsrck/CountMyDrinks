3f9d86c507e890a8a0aedd9b660849536e1f6545
Justin Park <j.park21@yahoo.com>
2017-04-29 23:58:45 -0400

    Fixed conflict between bac and reaction async task

--- app/src/main/java/com/mobile/countmydrinks/MainActivity.java
+++ app/src/main/java/com/mobile/countmydrinks/MainActivity.java
@@ -180,7 +180,7 @@ public class MainActivity extends AppCompatActivity
     public void startBACCalc() {
         if (!running) {
             timeAsyncTask = new TimeAsyncTask();
-            timeAsyncTask.execute();
+            timeAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
             running = true;
         }
     }
@@ -243,7 +243,7 @@ public class MainActivity extends AppCompatActivity
         protected Void doInBackground(Double... integers) {
             while (running) {
                 try {
-                    Thread.sleep(360000); // sleep for 6 minutes
+                    Thread.sleep(360000); // sleep for 6 minutes 360000
                 }
                 catch (Exception e) {
                     System.out.println(e);
